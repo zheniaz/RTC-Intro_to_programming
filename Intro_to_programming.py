248f72777f24c3d9cac45df2149c0b1ce919821c
zheniazgurovets <zhenia.zgurovets@gmail.com>
2025-07-14 15:33:09 -0700

    complete Week 1 assignments

---
+++ Intro_to_programming.py
@@ -0,0 +1,5 @@
+# Week 1
+miles_driven = int(input("Enter the number of miles driven: "))
+gallons_used = int(input("Enter the gallons of gas used: "))
+mpg = miles_driven / gallons_used
+print("Miles Per Gallon: " + str(mpg))
