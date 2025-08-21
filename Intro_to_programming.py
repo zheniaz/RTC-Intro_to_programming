4eed4f2f6d80f23704f765114d1784b50e67785b
zheniazgurovets <zhenia.zgurovets@gmail.com>
2025-08-21 10:52:11 -0700

    Removed unnecessary files

--- Intro_to_programming.py
+++
@@ -1,5 +0,0 @@
-# Week 1
-miles_driven = int(input("Enter the number of miles driven: "))
-gallons_used = int(input("Enter the gallons of gas used: "))
-mpg = miles_driven / gallons_used
-print("Miles Per Gallon: " + str(mpg))
